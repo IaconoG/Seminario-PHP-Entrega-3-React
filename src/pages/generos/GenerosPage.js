332d2997226da493004cd93c88d3b7fcef4c134c
iacono gianfranco <iacono.gian13@gmail.com>
2023-06-19 04:12:10 -0300

    creacion de archivos

---
+++ src/pages/generos/GenerosPage.js
@@ -0,0 +1,17 @@
+// Importacion de componentes
+import Header from '../../components/HeaderComponent';
+import Footer from '../../components/FooterComponent';
+
+const GenerosPage = () => {
+	return (
+		<>
+			<Header />
+			<div>
+					<h1>GenerosPage</h1>
+			</div>
+			<Footer />
+	</>
+	)
+}
+
+export default GenerosPage;
