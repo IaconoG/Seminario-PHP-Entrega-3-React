902b988f01dc8a0dbe2f599118f876ce7a5f4158
iacono gianfranco <iacono.gian13@gmail.com>
2023-06-19 04:11:45 -0300

    update

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
   // useEffect -> Hook de react que permite ejecutar codigo cuando se renderiza el componente
     // hook -> funcion que permite agregarle funcionalidad a un componente
   // useEffect -> se ejecuta después de que el componente se renderiza en el DOM
-import axios from 'axios';
 
 // Importacion de componentes
 import Header from '../../components/HeaderComponent';
@@ -17,6 +16,8 @@ import { obtenerGeneros } from '../../components/data/obtenerGeneros';
 import { obtenerPlataformas } from '../../components/data/obtenerPlataformas';
 import { obtenerJuegosFiltados } from '../../components/data/obtenerJuegosFiltrados';
 import { obtenerNombres } from '../../components/data/obtenerNombres';
+import { cargarData } from '../../components/data/cargarData.js';
+import { vaciarTabla } from '../../components/data/vaciarTabla.js'; 
 
 const  DashboardPage = () => {
   const baseURL = 'http://localhost:8000/public';
@@ -24,33 +25,89 @@ const  DashboardPage = () => {
   const [generos, setGeneros] = useState([]);
   const [plataformas, setPlataformas] = useState([]);
   const [nombres, setNombres] = useState([]);
-  const [errores, setErrores] = useState([]);
+  const [message, setMessage] = useState([]);
+  const [estadoBtnCargaDatos, setEstadoBtnCargaDatos] = useState(false);
+  const [estadoBtnEliminarDatos, setEstadoBtnEliminarDatos] = useState(false);
+  const [estadoAjusteSelects, setEstadoAjusteSelects] = useState(false);
 
   useEffect(() => {
-    // --- Obtener datos de la api ---
-    obtenerNombres(baseURL, setNombres, setErrores);
-    Promise.all([obtenerGeneros(baseURL, setGeneros, setErrores), obtenerPlataformas(baseURL, setPlataformas, setErrores)])
-    .then(() => {
-      ajustarSelects();
-    });
-      // Promise.all -> permite ejecutar varias promesas al mismo tiempo
-      // .then -> se ejecuta cuando todas las promesas se resuelven
-      // Esto nos garantiza que los estados generos y plataformas ya hayan sido actualizados antes de llamar a ajustarSelects().
-    obtenerJuegosFiltados(baseURL, setJuegos, setErrores, {nombre:'', plataforma:'', genero:'', orden: 'ASC'});
-  }, []); 
+    obtenerNombres(baseURL, setNombres);
+    Promise.all([ obtenerGeneros(baseURL, setGeneros),  obtenerPlataformas(baseURL, setPlataformas)
+      .then(() => { setEstadoAjusteSelects(true); })
+    ]);
+    obtenerJuegosFiltados(baseURL, setJuegos, {nombre:'', plataforma:'', genero:'', orden: 'ASC'});
+  }, []);
 
-  // === Evento envio del formulario ===
+  useEffect(() => {
+    if (estadoAjusteSelects) {
+      ajustarSelects();
+      setEstadoAjusteSelects(false);
+    }
+  }, [estadoAjusteSelects]);
+
+  useEffect(() => {
+    if (estadoBtnCargaDatos) {
+      Promise.all([ cargarData(baseURL, setMessage) ])
+        .then (() => {
+          Promise.all([ obtenerGeneros(baseURL, setGeneros),  obtenerPlataformas(baseURL, setPlataformas)
+            .then(() => { setEstadoAjusteSelects(true); })
+          ]);
+          obtenerJuegosFiltados(baseURL, setJuegos, {nombre:'', plataforma:'', genero:'', orden: 'ASC'});
+        });
+      setEstadoBtnCargaDatos(false);
+    }
+  }, [estadoBtnCargaDatos]); 
+
+  useEffect (() => {
+    if (estadoBtnEliminarDatos) {
+      // Promise.all([ vaciarTabla(baseURL, setMessage, 'plataformas'), vaciarTabla(baseURL, setMessage, 'generos')
+      //   .then(() => { 
+      //     Promise.all([ obtenerGeneros(baseURL, setGeneros),  obtenerPlataformas(baseURL, setPlataformas)
+      //       .then(() => { setEstadoAjusteSelects(true); })
+      //     ]);
+      //   })
+      // ]);
+      Promise.all([ vaciarTabla(baseURL, setMessage, 'juegos')
+        .then(() => { 
+          Promise.all([ obtenerGeneros(baseURL, setGeneros),  obtenerPlataformas(baseURL, setPlataformas)
+            .then(() => { setJuegos([]); })
+          ]);
+        })
+      ]);
+      setEstadoBtnEliminarDatos(false);
+    }
+  }, [estadoBtnEliminarDatos]);
+
+  useEffect(() => {
+    if (message.length > 0) {
+      for (let i = 0; i < message.length; i++) {
+        console.log(message[i]);
+      }
+      setMessage([]);
+    } 
+  }, [message]);
+  
+
   const handleSubmitFiltro = (event) => {
     event.preventDefault();
-    
     let nombre = event.target.elements.nombres.value;
     let plataforma = event.target.elements.plataformas.value;
     let genero = event.target.elements.generos.value;
     let orden = event.target.elements.ordenamiento.value;
-    
     // Llamada a la función de filtrarJuegos con los valores del formulario
-    obtenerJuegosFiltados(baseURL, setJuegos, setErrores, {nombre: nombre, plataforma: plataforma, genero: genero, orden: orden});
+    obtenerJuegosFiltados(baseURL, setJuegos, setMessage, {nombre: nombre, plataforma: plataforma, genero: genero, orden: orden});
+    // Mostrar msg de la api // FIXME: CREAR EL COSO EN EL NAVBAR PARA MOSTRAR EL MSJ
   };
+
+  // === Cargar datos a la BD ===
+  const handleCargarDatos = () => {
+    setEstadoBtnCargaDatos(true);
+  }
+  // === Eliminar juegos de la BD ===
+  const handleElimnarJuegos = () => {
+    setEstadoBtnEliminarDatos(true);
+  }
+
 
   return (
     <>
@@ -116,6 +173,13 @@ const  DashboardPage = () => {
             </div>
               <input type="submit" value="Filtrar"></input>
             </form>
+
+            <div className='contenedor-cargar-datos'>
+              <h3>Acciones</h3>
+              <input type="submit" value="Cargar Data" onClick={handleCargarDatos}></input>
+              <input type="submit" value="Eliminar Juegos" onClick={handleElimnarJuegos}></input>
+            </div>
+
           </aside>
           {/* Seccion que muestra el contenido */}
           <section className="juegos">
